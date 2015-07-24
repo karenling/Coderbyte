ba00fe27711a1fb7a961e9c2e0cdc4d6326391d2
Karen Ling <karensling@gmail.com>
2015-07-23 22:25:04 -0700

    Complete simple symbols.

---
+++ easy/simple_symbols.rb
@@ -0,0 +1,31 @@
+def SimpleSymbols(str)
+  letters = ("a".."z").to_a
+  str = str.split("")
+  str.each_with_index do |letter, idx|
+    if letters.include?(letter)
+      return false if idx - 1 < 0 || idx + 1 > str.length - 1
+      if str[idx-1] != "+" || str[idx+1] != "+"
+        return false
+      end
+    end
+  end
+  return true
+end
+p SimpleSymbols("+d+=3=+s+") #t
+p SimpleSymbols("f++d+") #f
+
+
+# def SimpleSymbols(str)
+#   letters = ("a".."z").to_a
+#
+#   str.split("").each_with_index do |chr, idx|
+#     if letters.include?(chr)
+#       return false if (idx-1 < 0) || (idx+1 > str.length - 1)
+#       if !(str[idx-1] == "+" && str[idx+1] == "+")
+#         return false
+#       end
+#     end
+#   end
+#
+#   return true
+# end
