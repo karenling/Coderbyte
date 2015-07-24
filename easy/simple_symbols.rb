67ecc17a3d30f8d35d703b656d0b939cf961a99b
Karen Ling <karensling@gmail.com>
2015-07-23 22:26:10 -0700

    Add note to simple symbols.

--- easy/simple_symbols.rb
+++ easy/simple_symbols.rb
@@ -11,13 +11,14 @@ def SimpleSymbols(str)
   end
   return true
 end
+
 p SimpleSymbols("+d+=3=+s+") #t
 p SimpleSymbols("f++d+") #f
 
 
 # def SimpleSymbols(str)
 #   letters = ("a".."z").to_a
-#
+# => coderbyte's ruby version doesn't allow this
 #   str.split("").each_with_index do |chr, idx|
 #     if letters.include?(chr)
 #       return false if (idx-1 < 0) || (idx+1 > str.length - 1)
