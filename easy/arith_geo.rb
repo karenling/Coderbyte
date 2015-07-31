15e42b26d237702e96d06b76ed7377cd6030e2c9
Karen Ling <karensling@gmail.com>
2015-07-31 11:27:30 -0700

    Complete arith geo.

--- easy/arith_geo.rb
+++ easy/arith_geo.rb
@@ -1,22 +1,34 @@
 def Arithmetic(arr)
-  i = 0
   diff = arr[1] - arr[0]
-  while i < arr.length
-    if arr[i + 1] - arr[i] == diff
-      next
-    else
-      geometric
-    i += 2
+
+  i = 0
+  while i < arr.length - 1
+    return false if arr[i + 1] - arr[i] != diff
+    i += 1
   end
+
+  return true
 end
 
+def Geometric(arr)
+  diff = arr[1] / arr[0]
+
+  i = 0
+  while i < arr.length - 1
+    return false if arr[i + 1] / arr[i] != diff
+    i += 1
+  end
+
+  return true
+end
 
 def ArithGeo(arr)
-
-
-
+  return "Arithmetic" if Arithmetic(arr)
+  return "Geometric" if Geometric(arr)
+  return "-1"
 end
 
 
-p Arithmetic([2, 4, 6, 8])
-p Arithmetic([2, 6, 18, 54])
+p ArithGeo([2, 4, 6, 8])
+p ArithGeo([2, 6, 18, 54])
+p ArithGeo([2, 6, 18, 5])
