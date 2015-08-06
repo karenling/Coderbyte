1ebf9452e5a7ea03c0d01d45dba23f8ec6980010
Karen Ling <karensling@gmail.com>
2015-08-06 02:07:00 -0700

    Complete swap case.

---
+++ easy/swap_case.rb
@@ -0,0 +1,16 @@
+def SwapCase(str)
+  str = str.split("").map do |char|
+    if char.downcase == char
+      char.upcase
+    elsif char.upcase == char
+      char.downcase
+    end
+  end
+
+  str.join("")
+
+end
+
+p SwapCase("Hello World") # "hELLO wORLD"
+p SwapCase("Hello-LOL") # "hELLO-lol"
+p SwapCase("Sup DUDE!!?") # "sUP dude!!?"
